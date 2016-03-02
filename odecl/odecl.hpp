c4c0561c243a9ddb859454e1dd10825665c26943
Eleftherios Avramidis <el.avramidis@gmail.com>
2016-03-02 20:36:50 +0100

    Added enumeration class build_Option. Enumerates the OpenCL build options.

--- odecl/odecl.hpp
+++ odecl/odecl.hpp
@@ -20,6 +20,7 @@
 #endif
 
 #include <odecl/timer.hpp>
+#include <odecl/build_Option.hpp>
 #include <odecl/output_Type.hpp>
 #include <odecl/device_Type.hpp>
 #include <odecl/solver_Type.hpp>
---
+++ odecl/odecl/build_Option.hpp
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2015,2016 Eleftherios Avramidis <el.avramidis@gmail.com>
+//
+// Distributed under The MIT License (MIT)
+// See accompanying file LICENSE.txt
+//---------------------------------------------------------------------------//
+
+#ifndef ODECL_BUILD_OPTION_HPP
+#define ODECL_BUILD_OPTION_HPP
+
+namespace odecl
+{
+	enum class build_Option
+	{
+		FastRelaxedMath = 1,
+		stdCL20 = 2
+	};
+}
+
+
+#endif // ODECL_BUILD_OPTION_HPP
