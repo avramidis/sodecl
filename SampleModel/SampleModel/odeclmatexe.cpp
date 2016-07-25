6e47faee012067eb064772dd0de3204d93ce0081
Eleftherios Avramidis <el.avramidis@gmail.com>
2016-07-25 21:29:46 +0300

    Added kernel string input in the odeclmatexe.cpp

--- SampleModel/SampleModel/odeclmatexe.cpp
+++ SampleModel/SampleModel/odeclmatexe.cpp
@@ -23,6 +23,9 @@ int main(int argc, char* argv[])
 	// device - OpenCL device number of selected platform
 	int a_device;
 
+	// Kernel filename with the ODE system definition
+	string a_system;
+
 	// initial - Initial conditions for each orbit for the ODE system
 	string a_initial;
 
@@ -58,14 +61,17 @@ int main(int argc, char* argv[])
 		// device - OpenCL device number of selected platform
 		a_device = atoi(argv[2]);
 
+		// Kernel filename with the ODE system definition
+		a_system = argv[3];
+
 		// initial - Initial conditions for each orbit for the ODE system
-		a_initial = argv[3];
+		a_initial = argv[4];
 
 		// parameters - Parameters sets for the ODE system for all orbits
-		a_parameters = argv[4];
+		a_parameters = argv[5];
 
 		// odesolver - ODE solver
-		switch (atoi(argv[5]))
+		switch (atoi(argv[6]))
 		{
 		case 0:
 			a_solver = odecl::solver_Type::Euler;
@@ -83,46 +89,41 @@ int main(int argc, char* argv[])
 		
 
 		// orbits - Number of orbits to be integrated
-		a_orbits = atoi(argv[6]);
+		a_orbits = atoi(argv[7]);
 
 		// nequats - Number of equations of the ODE system
-		a_equats = atoi(argv[7]);
+		a_equats = atoi(argv[8]);
 
 		// nparams - Number of parameters of the ODE system
-		a_nparams = atoi(argv[8]);
+		a_nparams = atoi(argv[9]);
 
 		// dt - ODE solver time step size
-		a_dt = atof(argv[9]);
+		a_dt = atof(argv[10]);
 
 		// tspan - Integration time span
-		a_tspan = atof(argv[10]);
+		a_tspan = atof(argv[11]);
 
 		// ksteps - Number of ODE solvers executed in each OpenCL kernel call
-		a_ksteps = atoi(argv[11]);
+		a_ksteps = atoi(argv[12]);
 	}
 	else
 	{
 		std::cout << "Number of argument is wrong." << std::endl;
 		std::cout << "Argument list:" << std::endl;
-		std::cout << "platform   - OpenCL platform number" << std::endl;
-		std::cout << "device - OpenCL device number of selected platform" << std::endl;
-		std::cout << "initial - Initial conditions for each orbit for the ODE system" << std::endl;
-		std::cout << "parameters - Parameters sets for the ODE system for all orbits" << std::endl;
-		std::cout << "orbits - Number of orbits to be integrated" << std::endl;
-		std::cout << "nequats - Number of equations of the ODE system" << std::endl;
-		std::cout << "nparams - Number of parameters of the ODE system" << std::endl;
-		std::cout << " dt - ODE solver time step size" << std::endl;
-		std::cout << " tspan - Integration time span" << std::endl;
-		std::cout << " ksteps - Number of ODE solvers executed in each OpenCL kernel call" << std::endl;
+		std::cout << "Argument 1  - OpenCL platform number" << std::endl;
+		std::cout << "Argument 2  - OpenCL device number of selected platform" << std::endl;
+		std::cout << "Argument 3  - Kernel filename with the ODE system definition" << std::endl;
+		std::cout << "Argument 4  - Initial conditions for each orbit for the ODE system" << std::endl;
+		std::cout << "Argument 5  - Parameters sets for the ODE system for all orbits" << std::endl;
+		std::cout << "Argument 6  - Number of orbits to be integrated" << std::endl;
+		std::cout << "Argument 7  - Number of equations of the ODE system" << std::endl;
+		std::cout << "Argument 8  - Number of parameters of the ODE system" << std::endl;
+		std::cout << "Argument 9  - ODE solver time step size" << std::endl;
+		std::cout << "Argument 10 - Integration time span" << std::endl;
+		std::cout << "Argument 11 - Number of ODE solvers executed in each OpenCL kernel call" << std::endl;
 	}
 
-	// Set the options of the ODE Solver
-	int list_size = 512;	// Population number
-	int equat_num = 6;		// ODE system number of equations
-	int param_num = 6;		// ODE system number of parameters
-
-
-	odecl::system *mysystem = new odecl::system("C:/Workfolder/ODECL_Repositories/odecl/odecl/kernels", "broomhead.cl", odecl::solver_Type::ImplicitEuler, a_dt, a_tspan, a_ksteps, a_equats, a_nparams, a_orbits, odecl::output_Type::File);
+	odecl::system *mysystem = new odecl::system("C:/Workfolder/ODECL_Repositories/odecl/odecl/kernels", &a_system[0], odecl::solver_Type::ImplicitEuler, a_dt, a_tspan, a_ksteps, a_equats, a_nparams, a_orbits, odecl::output_Type::File);
 
 	mysystem->set_outputfile("odecloutput.bin");
 
@@ -138,18 +139,12 @@ int main(int argc, char* argv[])
 
 	// Choose device
 	success = mysystem->choose_device(a_platform, odecl::device_Type::CPU, a_device);
-	//success=mysystem->choose_device(2, odecl::device_Type::GPU, 0); // W8100
-	//success = mysystem->choose_device(2, odecl::device_Type::GPU, 0); // K40c
 	if (success == 0)
 	{
 		cerr << "Error selecting OpenCL device!" << endl;
 	}
 
-	//mysystem->choose_device(2, odecl::device_Type::GPU, 0); // Firepro W8100
-	////mysystem->choose_device(2, odecl::device_Type::GPU, 1); // Radeon 7970
-	//mysystem->choose_device(3, odecl::device_Type::GPU, 0); // Tesla K40c
-
-	cl_double *t0 = new cl_double[list_size];
+	cl_double *t0 = new cl_double[a_orbits];
 	success = mysystem->read_data_from_file("x_t0.txt", t0);
 	if (success == 0)
 	{
@@ -157,7 +152,7 @@ int main(int argc, char* argv[])
 	}
 	mysystem->set_t0(t0);
 
-	cl_double *y0 = new cl_double[list_size*equat_num];
+	cl_double *y0 = new cl_double[a_orbits*a_equats];
 	success = mysystem->read_data_from_file("x_y0.txt", y0);
 	if (success == 0)
 	{
@@ -165,7 +160,7 @@ int main(int argc, char* argv[])
 	}
 	mysystem->set_y0(y0);
 
-	cl_double *params = new cl_double[list_size*param_num];
+	cl_double *params = new cl_double[a_orbits*a_nparams];
 	success = mysystem->read_data_from_file("x_params.txt", params);
 	if (success == 0)
 	{
