45e6b176e26eb7227adf766d303c0969198b4562
Eleftherios Avramidis <el.avramidis@gmail.com>
2017-09-05 17:22:44 +0100

    Added python script to call sodecl for the kuramoto model.

---
+++ pythonscripts/kuramotostoch.py
@@ -0,0 +1,76 @@
+# ---------------------------------------------------------------------------//
+#  Copyright (c) 2017 Eleftherios Avramidis <el.avramidis@gmail.com>
+#
+#  Distributed under The MIT License (MIT)
+#  See accompanying file LICENSE.txt
+# ---------------------------------------------------------------------------//
+
+import numpy, numpy.matlib
+import random
+import sodecl
+import time
+
+if __name__ ==  '__main__':
+
+	start = time.time()
+
+	orbits = 1
+	# orbits=512
+	# orbits=5120
+	# orbits=25600
+	# orbits=40960
+	# orbits=81920
+	# orbits=163840
+	# orbits=327680
+
+	openclplatform = 0
+	opencldevice = 0
+	openclkernel = 'kernels/kuramoto.cl'
+	solver = 0
+	nequat = 3
+	nparams = nequat
+	nnoi = nequat
+	dt = 5e-2
+	tspan = 200
+	ksteps = 40
+	localgroupsize = 0
+
+	# Initialise initial state of the system
+	initx = numpy.ndarray((orbits,nequat))
+	for o in range(orbits):
+		for p in range(nequat):
+			initx[o][p]=random.uniform(-3.1416, 3.1416)
+
+	# Initialise parameters values of the system
+	params = numpy.ndarray((orbits,nequat))
+	for o in range(orbits):
+		for p in range(nequat):
+			params[o][p]=random.uniform(0.01, 0.03)
+
+	# Initialise noise values of the system
+	noise = numpy.ndarray((orbits,nequat))
+	for o in range(orbits):
+		for p in range(nequat):
+			noise[o][p]=random.uniform(0.01, 0.03)
+
+	nparams = numpy.concatenate((params, noise), axis=1)
+
+	sodecl.sodecl(openclplatform, opencldevice, openclkernel,
+				initx, params, solver,
+				orbits, nequat, nnoi,
+				dt, tspan, ksteps, localgroupsize)
+
+	f = open("sodecloutput.bin", "r")
+	a = numpy.fromfile(f, dtype=numpy.float)
+
+	a = a.reshape(orbits, int(a.shape[0]/orbits), order='F')
+
+	end = time.time()
+	print("Simulation execution time: ", end - start, " seconds.")
+
+	print("NaN present: " + str(numpy.isnan(numpy.sum(numpy.sum(a)))))
+
+	import matplotlib.pyplot as plt
+	plt.plot(a[0,:])
+	plt.ylabel('some numbers')
+	plt.show()
