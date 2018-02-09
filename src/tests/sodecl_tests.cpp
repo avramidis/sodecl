8ddd44ab91826d2d2a83b529ff1d3e2b5b7c1f95
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-02-09 17:04:03 +0000

    Added unit test for creating OpenCL platform objects.

--- src/tests/sodecl_tests.cpp
+++ src/tests/sodecl_tests.cpp
@@ -8,7 +8,6 @@ TEST_CASE("Query OpenCL platforms")
     SECTION("Count OpenCL platforms test.")
     {
         sodecl::opencl_mgr m_opencl_mgr;
-
         int platforms_num = m_opencl_mgr.get_opencl_platform_count();
         
         CHECK(platforms_num != -1);
@@ -21,6 +20,19 @@ TEST_CASE("Query OpenCL platforms")
         if (platforms_num == 0) 
         {
             FAIL("No OpenCL platforms! Try to reinstall OpenCL runtime(s) for your device(s).");
+        }
+    }
+
+    SECTION("Create OpenCL platform objects.")
+    {
+        sodecl::opencl_mgr m_opencl_mgr;
+        int platforms_num = m_opencl_mgr.get_opencl_platform_count();
+        int status_value = m_opencl_mgr.create_opencl_platforms();
+
+        CHECK(status_value == 1);
+        if (status_value == -1) 
+        {
+            FAIL("Failed to generate the OpenCL platforms objects! Try to reinstall OpenCL runtime(s) for your device(s).");
         }
     }
 }
