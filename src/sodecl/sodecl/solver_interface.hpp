c1414d2e0fb43ce0fcd815d80a55035973648301
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-02-09 15:32:33 +0000

    Comment formatting for clarity.

--- src/sodecl/sodecl/solver_interface.hpp
+++ src/sodecl/sodecl/solver_interface.hpp
@@ -37,8 +37,8 @@ class solver_interface
      * @param	context		OpenCL cl_context.
      * @param	list_size	Size of the buffers in number of data points.
      * @param	equat_num	Number of equations.
-     * @param       param_num	Number of parameters.
-     * @return      int             Returns 1 if the operations were succcessfull or 0 if they were unsuccessful.
+     * @param   param_num	Number of parameters.
+     * @return  int         Returns 1 if the operations were succcessfull or 0 if they were unsuccessful.
      */
     virtual int create_buffers(cl_context context, int list_size, int equat_num, int param_num) = 0;
 
@@ -49,14 +49,14 @@ class solver_interface
      * @param	list_size	Number of data points to be written in the OpenCL memory buffers.
      * @param	equat_num	Number of equations of the ODE or SDE system.
      * @param	param_num	Number of parameters of the ODE or SDE system.
-     * @return      int             Returns 1 if the operations were succcessfull or 0 if they were unsuccessful.
+     * @return  int         Returns 1 if the operations were succcessfull or 0 if they were unsuccessful.
      */
     virtual int write_buffers(cl_command_queue commands, int list_size, int equat_num, int param_num) = 0;
 
     /**
      * @brief Executes the ODE or SDE solver on the selected OpenCL device.
      * 
-     * @return      int             Returns 1 if the operations were succcessfull or 0 if they were unsuccessful.
+     * @return  int         Returns 1 if the operations were succcessfull or 0 if they were unsuccessful.
      */
     virtual int run_ode_solver() = 0;
 };
