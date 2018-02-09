0a6c213cc195d1f84a9554cfb64cd64c338d39d2
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-02-09 16:15:47 +0000

    Added checks in the unit tests for better error checking.

--- src/tests/sodecl_tests.cpp
+++ src/tests/sodecl_tests.cpp
@@ -11,11 +11,13 @@ TEST_CASE("Query OpenCL platforms")
 
         int platforms_num = m_opencl_mgr.get_opencl_platform_count();
         
+        CHECK(platforms_num != -1);
         if (platforms_num == -1) 
         {
             FAIL("Failed to count the OpenCL platforms! Try to reinstall OpenCL runtime(s) for your device(s).");
         }
 
+        CHECK(platforms_num > 0);
         if (platforms_num == 0) 
         {
             FAIL("No OpenCL platforms! Try to reinstall OpenCL runtime(s) for your device(s).");
