99c058d15f871769d0e0d8214be170423c1d870d
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-02-09 14:50:22 +0000

    Added file with unit tests code.

---
+++ src/tests/sodecl_tests.cpp
@@ -0,0 +1,24 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "sodecl.hpp"
+
+TEST_CASE("Query OpenCL platforms")
+{
+    SECTION("Count OpenCL platforms test.")
+    {
+        sodecl::opencl_mgr m_opencl_mgr;
+
+        int platforms_num = m_opencl_mgr.get_platform_count();
+        
+        if (platforms_num == -1) 
+        {
+            FAIL("Failed to count the OpenCL platforms! Try to reinstall OpenCL.");
+        }
+
+        if (platforms_num == 0) 
+        {
+            FAIL("No OpenCL platforms! Try to reinstall OpenCL.");
+        }
+    }
+}
