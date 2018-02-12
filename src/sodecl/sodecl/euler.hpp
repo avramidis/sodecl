8bb801c16815b08619a96d69640a348f13957ff5
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-02-12 09:12:59 +0000

    Converted euler class from virtual pure to normal.

--- src/sodecl/sodecl/euler.hpp
+++ src/sodecl/sodecl/euler.hpp
@@ -10,26 +10,30 @@ namespace sodecl
  * @brief Abstract class that acts as an interface for the implementation of different ODE or SDE solvers.
  * 
  */
-class euler : solver_interface
+class euler : public solver_interface
 {
     /**
      * @brief Default constructor.
      * 
      */
-    euler(){};
+    euler() : solver_interface()
+    {
+    }
 
     /**
      * @brief Virtual destructor.
      * 
      */
-    virtual ~euler(){};
+    ~euler()
+    {
+    }
 
     /**
      * @brief Forms the OpenCL kernel string(s).
      * 
      * @return  int         Returns 1 if the operations were succcessful or 0 if they were unsuccessful.
      */
-    virtual int create_kernel_string() = 0;
+    int create_kernel_string(){};
 
     /**
      * @brief Creates the OpenCL buffers needed for the solver.
@@ -40,7 +44,9 @@ class euler : solver_interface
      * @param   param_num	Number of parameters.
      * @return  int         Returns 1 if the operations were succcessful or 0 if they were unsuccessful.
      */
-    virtual int create_buffers(cl_context context, int list_size, int equat_num, int param_num) = 0;
+    int create_buffers(cl_context context, int list_size, int equat_num, int param_num)
+    {
+    }
 
     /**
      * @brief Writes data in the OpenCL memory buffers.
@@ -51,7 +57,9 @@ class euler : solver_interface
      * @param	param_num	Number of parameters of the ODE or SDE system.
      * @return  int         Returns 1 if the operations were succcessful or 0 if they were unsuccessful.
      */
-    virtual int write_buffers(cl_command_queue commands, int list_size, int equat_num, int param_num) = 0;
+    int write_buffers(cl_command_queue commands, int list_size, int equat_num, int param_num)
+    {
+    }
 
     /**
      * Setups the selected ODE or SDE solver OpenCL kernel source.
@@ -63,14 +71,18 @@ class euler : solver_interface
      * 
      * @return  int         Returns 1 if the operations were succcessful or 0 if they were unsuccessful.
      */
-    virtual int setup_solver() = 0;
+    int setup_solver()
+    {
+    }
 
     /**
      * @brief Executes the ODE or SDE solver on the selected OpenCL device.
      * 
      * @return  int         Returns 1 if the operations were succcessful or 0 if they were unsuccessful.
      */
-    virtual int run_solver() = 0;
+    int run_solver()
+    {
+    }
 };
 }
 
