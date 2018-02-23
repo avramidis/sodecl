72796359c6ad5e19373cdd222b159cf83347c68e
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-02-23 19:52:24 +0000

    The benchmark results are written in txt files.

--- src/pythonscripts/kuramotobenchmarks.py
+++ src/pythonscripts/kuramotobenchmarks.py
@@ -11,8 +11,10 @@ import numpy
 import sodecl
 
 if __name__ == '__main__':
-    orbit_set = [512, 5120, 25600, 40960, 81920, 163840, 327680]
+    # orbit_set = [512, 5120, 25600, 40960, 81920, 163840, 327680]
+    orbit_set = [512, 5120]
     nequat_set = [3, 10, 20]
+    repetitions = 3
 
     openclplatform = 0
     opencldevice = 0
@@ -22,9 +24,8 @@ if __name__ == '__main__':
     tspan = 200
     ksteps = 40
     localgroupsize = 8
-    repetitions = 8
 
-    runtimes = numpy.zeros((len(nequat_set), repetitions, len(orbit_set)))
+    runtimes = numpy.zeros((len(nequat_set), len(orbit_set), repetitions+1))
 
     orbits = 512
     nequat_count = 0
@@ -32,7 +33,7 @@ if __name__ == '__main__':
         orbits_count = 0
         for orbits in orbit_set:
             reps_count = 0
-            for reps in range(7):
+            for reps in range(repetitions):
                 time.sleep(1)
                 start_time = time.time()
                 
@@ -66,14 +67,22 @@ if __name__ == '__main__':
 
                 end_time = time.time()
 
-                runtimes[nequat_count][orbits_count][reps_count] = end_time - start_time
-                reps_count = reps_count + 1
                 #print("Simulation execution time: ", end_time - start_time, " seconds.")
-
                 if numpy.isnan(numpy.sum(numpy.sum(results))):
                     raise RuntimeError("NaN present!")
+
+                runtimes[nequat_count][orbits_count][reps_count] = end_time - start_time
+                reps_count = reps_count + 1
+            
+            runtimes[nequat_count][orbits_count][reps_count] = numpy.mean(runtimes[nequat_count][orbits_count][0:repetitions-1])
             
             orbits_count = orbits_count + 1
         nequat_count = nequat_count + 1
 
     print(runtimes)
+
+    #####################################################
+    ## Write the results to files
+    numpy.savetxt('nequat_3.txt', runtimes[0][:][:], fmt='%.4f')
+    numpy.savetxt('nequat_10.txt', runtimes[1][:][:], fmt='%.4f')
+    numpy.savetxt('nequat_20.txt', runtimes[2][:][:], fmt='%.4f')
