c13566dc542af62338e4dae7be634fbe1760871d
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-05-19 10:04:59 +0100

    Added tests for initial OpenCL functions (e.g. OpenCL platforms).

--- src/tests/sodecl_tests.cpp
+++ src/tests/sodecl_tests.cpp
@@ -7,72 +7,72 @@ TEST_CASE("Test OpenCL platforms.")
 {
     SECTION("Count OpenCL platforms test.")
     {
-        // sodecl::opencl_mgr m_opencl_mgr;
-        // int platforms_num = m_opencl_mgr.get_opencl_platform_count();
+        sodecl::opencl_mgr m_opencl_mgr;
+        int platforms_num = m_opencl_mgr.get_opencl_platform_count();
         
-        // CHECK(platforms_num != -1);
-        // if (platforms_num == -1) 
-        // {
-        //     FAIL("Failed to count the OpenCL platforms! Try to reinstall OpenCL runtime(s) for your device(s).");
-        // }
+        CHECK(platforms_num != -1);
+        if (platforms_num == -1) 
+        {
+            FAIL("Failed to count the OpenCL platforms! Try to reinstall OpenCL runtime(s) for your device(s).");
+        }
 
-        // CHECK(platforms_num > 0);
-        // if (platforms_num == 0) 
-        // {
-        //     FAIL("No OpenCL platforms! Try to reinstall OpenCL runtime(s) for your device(s).");
-        // }
+        CHECK(platforms_num > 0);
+        if (platforms_num == 0) 
+        {
+            FAIL("No OpenCL platforms! Try to reinstall OpenCL runtime(s) for your device(s).");
+        }
     }
 
-    // SECTION("Create OpenCL platform objects.")
-    // {
-    //     sodecl::opencl_mgr m_opencl_mgr;
-    //     int platforms_num = m_opencl_mgr.get_opencl_platform_count();
-    //     int status_value = m_opencl_mgr.create_opencl_platforms();
+    SECTION("Create OpenCL platform objects.")
+    {
+        sodecl::opencl_mgr m_opencl_mgr;
+        int platforms_num = m_opencl_mgr.get_opencl_platform_count();
+        int status_value = m_opencl_mgr.create_opencl_platforms();
 
-    //     CHECK(status_value == 1);
-    //     if (status_value == -1) 
-    //     {
-    //         FAIL("Failed to generate the OpenCL platforms objects! Try to reinstall OpenCL runtime(s) for your device(s).");
-    //     }
-    // }
+        CHECK(status_value == 1);
+        if (status_value == -1) 
+        {
+            FAIL("Failed to generate the OpenCL platforms objects! Try to reinstall OpenCL runtime(s) for your device(s).");
+        }
+    }
 }
 
-// TEST_CASE("Test choosing the first OpenCL device.")
-// {
-//     SECTION("Choose the first OpenCL device of the first OpenCL platform.")
-//     {
-//         sodecl::opencl_mgr m_opencl_mgr;
-//         int platforms_num = m_opencl_mgr.get_opencl_platform_count();
-//         int status_value = m_opencl_mgr.create_opencl_platforms();
+TEST_CASE("Test choosing the first OpenCL device.")
+{
+    SECTION("Choose the first OpenCL device of the first OpenCL platform.")
+    {
+        sodecl::opencl_mgr m_opencl_mgr;
+        int platforms_num = m_opencl_mgr.get_opencl_platform_count();
+        int status_value = m_opencl_mgr.create_opencl_platforms();
 
-//         status_value = m_opencl_mgr.choose_opencl_device(0, sodecl::device_Type::ALL, 0);
+        status_value = m_opencl_mgr.choose_opencl_device(0, sodecl::device_Type::ALL, 0);
 
-//         CHECK(status_value == 1);
-//         if (status_value == -1) 
-//         {
-//             FAIL("Failed to choose the OpenCL device selected!");
-//         }
-//     }
-// }
+        CHECK(status_value == 1);
+        if (status_value == -1) 
+        {
+            FAIL("Failed to choose the OpenCL device selected!");
+        }
+    }
+}
 
-// TEST_CASE("Test creation of OpenCL context.")
-// {
-//     SECTION("Create OpenCL context.")
-//     {
-//         sodecl::opencl_mgr m_opencl_mgr;
-//         int platforms_num = m_opencl_mgr.get_opencl_platform_count();
-//         int status_value = m_opencl_mgr.create_opencl_platforms();
-//         status_value = m_opencl_mgr.choose_opencl_device(0, sodecl::device_Type::ALL, 0);
+TEST_CASE("Test creation of OpenCL context.")
+{
+    SECTION("Create OpenCL context.")
+    {
+        sodecl::opencl_mgr m_opencl_mgr;
+        int platforms_num = m_opencl_mgr.get_opencl_platform_count();
+        int status_value = m_opencl_mgr.create_opencl_platforms();
+        status_value = m_opencl_mgr.choose_opencl_device(0, sodecl::device_Type::ALL, 0);
 
-//         status_value = m_opencl_mgr.create_context();
+        status_value = m_opencl_mgr.create_context();
 
-//         CHECK(status_value == 1);
-//         if (status_value == -1) 
-//         {
-//             FAIL("Failed to create the OpenCL context for the selected platform and device!");
-//         }
-//     }
-// }
+        CHECK(status_value == 1);
+        if (status_value == -1) 
+        {
+            FAIL("Failed to create the OpenCL context for the selected platform and device!");
+        }
+    }
+}
 
 // TEST_CASE("Test creation of OpenCL kernel.")
 // {
